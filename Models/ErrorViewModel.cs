14520ea57fb6d1cad4951fc076006e7fced5448a
sergioAscurra-10 <sebastianperez.27102005@gmail.com>
2025-09-26 19:53:08 -0500

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PortalInmobiliario.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
